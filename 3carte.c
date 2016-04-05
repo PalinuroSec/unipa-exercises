3f1323c204bbe064820019760b7eb5c48ec55d44
EclipseSpark <eclipse@frozenbox.org>
2016-04-05 23:22:31 +0200

    add 3carte.c

---
+++ 3carte.c
@@ -0,0 +1,28 @@
+/*
+gioco delle 3 carte
+*/
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  int position, x;
+  srand(time(NULL));
+  position = rand()%3+1;
+
+  printf("gioco delle 3 carte\n\n");
+  printf("indovina dove si trova la carta vincente\n");
+  printf("indica il tuo numero vincente [1] [2] [3] -> ");
+  scanf("%d", &x);
+  if (x == position)
+    {
+      printf("hai indovinato!\n");
+    }
+  else
+    {
+      printf("hai perso, la carta vincente era la %d\n", position);
+    }
+  return 0;
+}
