083b407f3e0094b6799e3457396dadb07681959e
EclipseSpark <eclipse@frozenbox.org>
2016-04-05 23:24:19 +0200

    improve 3carte.c

--- 3carte.c
+++ 3carte.c
@@ -10,19 +10,22 @@ int main(int argc, char **argv)
 {
   int position, x;
   srand(time(NULL));
-  position = rand()%3+1;
 
-  printf("gioco delle 3 carte\n\n");
-  printf("indovina dove si trova la carta vincente\n");
-  printf("indica il tuo numero vincente [1] [2] [3] -> ");
-  scanf("%d", &x);
-  if (x == position)
+  while(x != 0)
     {
-      printf("hai indovinato!\n");
-    }
-  else
-    {
-      printf("hai perso, la carta vincente era la %d\n", position);
+      position = rand()%3+1;
+      printf("gioco delle 3 carte\n\n");
+      printf("indovina dove si trova la carta vincente\n");
+      printf("indica il tuo numero vincente [1] [2] [3] -> ");
+      scanf("%d", &x);
+      if (x == position)
+	{
+	  printf("hai indovinato!\n");
+	}
+      else
+	{
+	  printf("hai perso, la carta vincente era la %d\n", position);
+	}
     }
   return 0;
 }
