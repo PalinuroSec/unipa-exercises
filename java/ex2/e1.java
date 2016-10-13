cfdb0f5ba4b342bee67ac7f4b35c91a1124736d3
EclipseSpark <eclipse@frozenbox.org>
2016-10-13 12:11:21 +0200

    upload second java exercises

---
+++ java/ex2/e1.java
@@ -0,0 +1,22 @@
+public class ex1
+{
+    public static void main(String[] args){
+	int a = 5;
+	int b = 3;
+	float r1 = (float) a/b;
+	System.out.println(r1);
+
+	char c = 'a';
+	short s = 5000;
+	int r2 = (int) a * s;
+	System.out.println(r2);
+
+	int i = 6;
+	float f = 3.14F;
+	float r3 = i + f;
+	System.out.println(r3);
+
+	float r4 = r1 - r2 - r3;
+	System.out.println(r4);
+    }
+}
